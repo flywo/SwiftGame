5cec4f731e528f16b1e8b88dea24e6c45cf150a1
flywo <316910279@qq.com>
2017-01-24 16:50:27 +0800

    add gif

--- Game01/Game/ShipScene.swift
+++ Game01/Game/ShipScene.swift
@@ -27,6 +27,30 @@ class ShipScene: SKScene {
         let ship = newShip()
         ship.position = CGPoint(x: frame.width/2, y: frame.height/2)
         addChild(ship)
+        
+        //产生岩石
+        let makeRocks = SKAction.sequence([SKAction.perform(#selector(addRock), onTarget: self),
+                                           SKAction.wait(forDuration: 0.1, withRange: 0.15)])
+        run(SKAction.repeatForever(makeRocks))
+    }
+    
+    //添加岩石
+    func addRock() {
+        let rock = SKSpriteNode(color: .white, size: CGSize(width: 10, height: 10))
+        rock.position = CGPoint(x: frame.width*2/3, y: frame.height)
+        rock.name = "rock"
+        rock.physicsBody = SKPhysicsBody(rectangleOf: rock.size)
+        rock.physicsBody?.usesPreciseCollisionDetection = true
+        addChild(rock)
+    }
+    
+    //岩石离开屏幕后，移除
+    override func didSimulatePhysics() {
+        enumerateChildNodes(withName: "rock") { (node, _) in
+            if node.position.y < 0 {
+                node.removeFromParent()
+            }
+        }
     }
     
     func newShip() -> SKSpriteNode {
@@ -36,6 +60,9 @@ class ShipScene: SKScene {
                                        SKAction.moveBy(x: 100, y: 50, duration: 1),
                                        SKAction.wait(forDuration: 1),
                                        SKAction.moveBy(x: -100, y: 150, duration: 1)])
+        //给飞船添加物理体
+        node.physicsBody = SKPhysicsBody(rectangleOf: node.size)
+        node.physicsBody?.isDynamic = false//防止非常受物理交互影响，开启后，飞船的速度不会受物理碰撞影响
         node.run(hover)
         
         //飞船添加亮光，亮光添加到飞船上后，飞船移动后，亮光也会一起移动，旋转等也是。
